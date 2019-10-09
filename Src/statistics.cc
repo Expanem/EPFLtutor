84b9322d20430e0de3aac866ad88847912b8e02c
Goneiross <guillaume.julien.pellerin@gmail.com>
2019-10-09 21:49:02 +0200

    Add function to compute average

--- Src/statistics.cc
+++ Src/statistics.cc
@@ -2,3 +2,36 @@
   \file   statistics.cc
   \brief  Analyse data for the user.
 */
+
+#include "statistics.h"
+#include "configuration.h"
+#include "dataManager.h"
+
+using namespace std;
+
+float average(CourseData course) {
+  /** Return the average of a course **/
+  int sum = 0;
+  for (size_t i = 0; i < course.getSerie().size()) {
+    sum += average(course.getSerie()[i]);
+  }
+  return sum /= course.getSerie().size();
+}
+
+float average(Serie* serie) {
+  /** Return the average of a serie' marks **/
+  int sum = 0;
+  for (size_t i = 0; i < serie->getExercises().size()) {
+    sum += serie->getExercises()[i]->getMark();
+  }
+  return sum /= serie->getExercises().size();
+}
+
+float average(vector<float> numbers) {
+  /** Return the average of a list **/
+  int sum = 0;
+  for (size_t i = 0; i < numbers.size(); i++) {
+    sum += numbers[i];
+  }
+  return sum / numbers.size();
+}
--- Src/statistics.h
+++ Src/statistics.h
@@ -2,3 +2,7 @@
   \file   statistics.h
   \brief  Analyse data for the user.
 */
+
+float average(CourseData course);
+float average(Serie* serie);
+float average(vector<float> numbers);
